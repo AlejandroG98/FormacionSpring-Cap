cea6c08237d095228906cab328d9bbef627777ec
Garcia Verdejo <alejandro.garcia-verdejo@capgemini.com>
2023-03-24 08:41:44 +0100

    Primera prueba con Mockito

--- demo/src/main/java/com/example/ejemplos/Persona.java
+++ demo/src/main/java/com/example/ejemplos/Persona.java
@@ -1,12 +1,40 @@
 package com.example.ejemplos;
 
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
 public class Persona {
-	private int id;
-	private String nombre;
-	private String apellidos;
+
+	int id;
+	String nombre;
+	String apellidos;
+
+	public Persona(int id, String nombre, String apellidos) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
 }
--- demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
+++ demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
@@ -2,6 +2,8 @@ package com.example.ejemplos;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -63,6 +65,14 @@ class CalculadoraTest {
 			@CsvSource(value = {"1,1,2","0.1,0.2,0.3","1,2,3","0,0,0","-1,-1,-2"})
 			void test_Suma_Parametrizada(double op1, double op2, double rslt) {
 				assertEquals(Math.floor(rslt), Math.floor(calc.suma(op1, op2)));
+			}
+			
+			// Test con Mock
+			@Test
+			void testSumaMock(){
+				Calculadora calc = mock(Calculadora.class);
+				when(calc.suma(2, 2)).thenReturn(3.0);
+				assertEquals(3, calc.suma(2, 2));
 			}
 		}
 
