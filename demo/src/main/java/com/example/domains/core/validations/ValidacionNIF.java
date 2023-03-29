84b66e4a78a6b3a26c2ccaddc983d18f0f42d4a1
Alejandro <alejandrogarciav3098@gmail.com>
2023-03-29 09:30:48 +0200

    Spring Data v0.3

---
+++ demo/src/main/java/com/example/domains/core/validations/ValidacionNIF.java
@@ -0,0 +1,7 @@
+package com.example.domains.core.validations;
+
+public class ValidacionNIF {
+	public boolean NIFcheck(String NIF) {
+		return NIF.length() != 9 || !NIF.matches("\\d{8}[a-zA-Z]") ? false :  true;
+	}
+}
---
+++ demo/src/test/java/com/example/domains/core/validations/ValidacionNIFTest.java
@@ -0,0 +1,39 @@
+package com.example.domains.core.validations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+class ValidacionNIFTest  {
+
+	ValidacionNIF nif;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		nif = new ValidacionNIF();
+	}
+
+	@Nested
+	@DisplayName("Pruebas para saber si es valido o no un NIF")
+	@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+	class isNIF {
+
+		@ParameterizedTest
+		@ValueSource(strings = {"12345678A", "12345679F"})
+		void casosValidos(String NIF) {
+			assertAll("Validar propiedades", 
+					() -> assertNotNull(nif.NIFcheck(NIF)),
+					() -> assertTrue(nif.NIFcheck(NIF))
+					);
+		}
+
+		@ParameterizedTest
+		@ValueSource(strings = {"12345678", "1234567A","12345678AA","1234567AA"})
+		void casosInvalidos(String NIF) {
+			assertAll("Validar propiedades", 
+					() -> assertNotNull(nif.NIFcheck(NIF)), 
+					() -> assertFalse(nif.NIFcheck(NIF)));
+		}
+	}
+}
