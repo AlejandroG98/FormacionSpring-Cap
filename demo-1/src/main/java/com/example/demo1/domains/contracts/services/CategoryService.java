1838ec899ddbfbb5a3fdef5402c89196d9677bf3
Alejandro <108611527+AlejandroG98@users.noreply.github.com>
2023-03-30 15:18:50 +0200

    Cambios

--- demo-1/src/main/java/com/example/demo1/domains/contracts/services/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo1.domains.contracts.services;
-
-import com.example.demo1.domains.core.services.contracts.ProjectionDomainService;
-import com.example.demo1.domains.entities.Category;
-
-public interface CategoryService extends ProjectionDomainService<Category, Integer>{
-
-}
--- demo-1/src/main/java/com/example/demo1/domains/contracts/services/LanguageService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo1.domains.contracts.services;
-
-import com.example.demo1.domains.core.services.contracts.ProjectionDomainService;
-import com.example.demo1.domains.entities.Language;
-
-public interface LanguageService extends ProjectionDomainService<Language, Integer>{
-
-}
