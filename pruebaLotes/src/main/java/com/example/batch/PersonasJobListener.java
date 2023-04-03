d75f35ab7345f4deda3529b5226a94161599e411
Alejandro <alejandrogarciav3098@gmail.com>
2023-04-03 12:18:59 +0200

    De CSV a DB

--- pruebaLotes/src/main/java/com/example/batch/PersonasJobListener.java
+++ pruebaLotes/src/main/java/com/example/batch/PersonasJobListener.java
@@ -1,14 +1,43 @@
 package com.example.batch;
 
+import javax.sql.DataSource;
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
 import com.example.model.Persona;
+import com.example.model.PersonaDTO;
 
 @Component
 public class PersonasJobListener implements JobExecutionListener {
@@ -25,5 +54,26 @@ public class PersonasJobListener implements JobExecutionListener {
 							(rs, row) -> new Persona(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getString(4)))
 					.forEach(p -> log.info("Fila: " + p));
 		}
+	}
+
+	public FlatFileItemReader<PersonaDTO> personaCSVItemReader(String fname) {
+		return new FlatFileItemReaderBuilder<PersonaDTO>().name("personaCSVItemReader")
+				.resource(new ClassPathResource(fname)).linesToSkip(1).delimited()
+				.names(new String[] { "id", "nombre", "apellidos", "correo", "sexo", "ip" })
+				.fieldSetMapper(new BeanWrapperFieldSetMapper<PersonaDTO>() {
+					{
+						setTargetType(PersonaDTO.class);
+					}
+				}).build();
+	}
+
+	@Autowired
+	public PersonaItemProcessor personaItemProcessor;
+
+	@Bean
+	public JdbcBatchItemWriter<Persona> personaDBItemWriter(DataSource dataSource) {
+		return new JdbcBatchItemWriterBuilder<Persona>()
+				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+				.sql("INSERT INTO personas VALUES (:id,:nombre,:correo,:ip)").dataSource(dataSource).build();
 	}
 }
