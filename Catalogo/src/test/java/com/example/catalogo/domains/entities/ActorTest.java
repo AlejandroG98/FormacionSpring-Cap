ca03e4524b0e30927407aefc217c9e23b55aeaf1
Alejandro <alejandrogarciav3098@gmail.com>
2023-04-03 12:28:45 +0200

    Test Mejorados en Category

--- Catalogo/src/test/java/com/example/catalogo/domains/entities/ActorTest.java
+++ Catalogo/src/test/java/com/example/catalogo/domains/entities/ActorTest.java
@@ -1,16 +1,12 @@
 package com.example.catalogo.domains.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.math.BigDecimal;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import com.example.domains.entities.*;
-import com.example.domains.entities.Film.Rating;
 
 class ActorTest {
 
@@ -19,23 +15,7 @@ class ActorTest {
 		var fixture = new Actor(0, "Pepito", "GRILLO");
 		assertTrue(fixture.isValid());
 	}
-	
-//	@DisplayName("El apellido deben tener entre 2 y 45 caracteres, y no pueden estar en blanco")
-//    @ParameterizedTest(name = "lastName: -{0}- -> {1}")
-//    @CsvSource(value = {
-//        "'','ERRORES: lastName: Tiene que estar en mayusculas. lastName: size must be between 2 and 45.'",
-//        "' ','ERRORES: lastName: Tiene que estar en mayusculas. lastName: size must be between 2 and 45.'",
-//        "'   ','ERRORES: lastName: Tiene que estar en mayusculas.'",
-//        "A,'ERRORES: lastName: size must be between 2 and 45.'",
-//        "12345678901234567890123456789012345678901234567890,'ERRORES: lastName: Tiene que estar en mayusculas. lastName: size must be between 2 and 45.'"
-//    	})
-//    void testLastNameIsInvalid(String valor, String error) {
-//        var fixture = new Actor(0, "Pepito", valor);
-//        assertNotNull(fixture);
-//        assertTrue(fixture.isInvalid());
-//        assertEquals(error, fixture.getErrorsMessage());
-//    }
-	
+		
 	@Nested
 	class Name {
 		@Nested
--- Catalogo/src/test/java/com/example/catalogo/domains/entities/CategoryTest.java
+++ Catalogo/src/test/java/com/example/catalogo/domains/entities/CategoryTest.java
@@ -3,6 +3,7 @@ package com.example.catalogo.domains.entities;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -15,20 +16,42 @@ class CategoryTest {
 		var fixture = new Category(0, "Comedy");
 		assertTrue(fixture.isValid());
 	}
+	
+	@Nested
+	class Name {
+		@Nested
+		class OK {
+			@DisplayName("El nombre no está en blanco")
+			@ParameterizedTest(name = "Name: {0}, Error: {1}")
+			@CsvSource(value = { 
+				"'Western'",
+				"'Acción'",
+				"'Policias" 
+				})
+			void testName(String valor) {
+				var fixture = new Category(0, valor);
+				assertNotNull(fixture);
+				assertTrue(fixture.isValid());
+			}
+		}
 
-	@DisplayName("El nombre no puede estar en blanco")
-	@ParameterizedTest(name = "Name: {0}, Error: {1}")
-	@CsvSource(value = { 
-		"'','ERRORES: name: must not be blank.'",
-		"' ','ERRORES: name: must not be blank.'",
-		"'   ','ERRORES: name: must not be blank.'", 
-		"12345678901234567890123456789012345678901234567890,'ERRORES: name: size must be between 0 and 25.'" 
-		})
-	void testNameIsInvalid(String valor, String error) {
-		var fixture = new Category(0, valor);
-		assertNotNull(fixture);
-		assertTrue(fixture.isInvalid());
-		assertEquals(error, fixture.getErrorsMessage());
+		@Nested
+		class KO {
+			@DisplayName("El nombre no puede estar en blanco")
+			@ParameterizedTest(name = "Name: {0}, Error: {1}")
+			@CsvSource(value = { 
+				"'','ERRORES: name: must not be blank.'",
+				"' ','ERRORES: name: must not be blank.'",
+				"'   ','ERRORES: name: must not be blank.'", 
+				"12345678901234567890123456789012345678901234567890,'ERRORES: name: size must be between 0 and 25.'" 
+				})
+			void testName(String valor, String error) {
+				var fixture = new Category(0, valor);
+				assertNotNull(fixture);
+				assertTrue(fixture.isInvalid());
+				assertEquals(error, fixture.getErrorsMessage());
+			}
+		}
 	}
 
 }
