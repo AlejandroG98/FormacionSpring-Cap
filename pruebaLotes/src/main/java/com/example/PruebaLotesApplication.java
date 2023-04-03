539b2a985842c12931bac4fa4c0b1df0d08f2769
Alejandro <alejandrogarciav3098@gmail.com>
2023-04-03 11:58:23 +0200

    Creamos Persona y su DTO en el package Model

---
+++ pruebaLotes/src/main/java/com/example/PruebaLotesApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaLotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaLotesApplication.class, args);
+	}
+
+}
---
+++ pruebaLotes/src/main/java/com/example/model/Persona.java
@@ -0,0 +1,14 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Persona {
+
+	private long id;
+	private String nombre, correo, ip;
+	// Ctor(), Get/Set, toString(), …
+	
+}
