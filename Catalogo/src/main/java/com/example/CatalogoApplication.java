73733a1629d0924e8cae62dcd9901b5aaa57d1a0
Alejandro <alejandrogarciav3098@gmail.com>
2023-04-12 10:50:41 +0200

    Configuración para Swagger-UI (OpenAPI)

--- Catalogo/src/main/java/com/example/CatalogoApplication.java
+++ Catalogo/src/main/java/com/example/CatalogoApplication.java
@@ -1,17 +1,12 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.domains.contracts.services.CategoryService;
-import com.example.domains.contracts.services.LanguageService;
-import com.example.domains.entities.Category;
-import com.example.domains.entities.Film;
-import com.example.domains.entities.FilmCategory;
+import org.springframework.context.annotation.*;
 import jakarta.transaction.Transactional;
-import java.util.List;
+import java.util.TreeMap;
 
 @SpringBootApplication
 public class CatalogoApplication implements CommandLineRunner {
@@ -20,7 +15,6 @@ public class CatalogoApplication implements CommandLineRunner {
 		SpringApplication.run(CatalogoApplication.class, args);
 	}
 
-
 	@Override
 	@Transactional
 	public void run(String... args) throws Exception {
@@ -28,4 +22,11 @@ public class CatalogoApplication implements CommandLineRunner {
 
 	}
 
+	@Bean
+	OpenApiCustomizer sortSchemasAlphabetically() {
+		return openApi -> {
+			var schemas = openApi.getComponents().getSchemas();
+			openApi.getComponents().setSchemas(new TreeMap<>(schemas));
+		};
+	}
 }
