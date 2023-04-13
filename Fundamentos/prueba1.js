9c001146015bac72eef1fe0823f3ae13db351dd8
Alejandro <alejandrogarciav3098@gmail.com>
2023-04-13 14:43:07 +0200

    Primera prueba de Arrays y Objeto en JavaScript

--- Fundamentos/prueba1.js
+++ Fundamentos/prueba1.js
@@ -1,8 +1,8 @@
 // Imprime por pantalla los indices y no los valores
-t = [10,20,30]
-for(v in t)
+let t = [10,20,30]
+for(i in t)
 {
-    console.log(v);
+    console.log(i);
 }
 
 // Imprime por pantalla los valores y no los indices
@@ -14,11 +14,37 @@ for(v of t)
 
 function suma(a,b) {return a+b}
 
-filtrado = function(item){
+function filtrado(item, a){
     return item > a;
 }
 
-for(let i=0; i<10; i++)
-{
-    t.filter(filtrado);
+t = [10, 20, 30];
+
+for(let i = 0; i < 10; i++){
+    t = t.filter(item => filtrado(item, i)); 
 }
+
+console.log(t); 
+
+// ARRAYS
+let tab = new Array();
+tab = [10,20,30];
+tab[10]= 'otro';
+// Indices
+for(i in tab){
+    console.log(tab[i])
+}
+// Valores
+for(v of tab){
+    console.log(v)
+}
+
+let o = new Object();
+o = {
+  nombre: 'Pepito',
+  apellidos: 'Grillo',
+  nom: function() {
+    return this.nombre + ' ' + this.apellidos;
+  }
+}
+console.log(`${o.nombre} ${o.apellidos} ${o.nom()}`);
