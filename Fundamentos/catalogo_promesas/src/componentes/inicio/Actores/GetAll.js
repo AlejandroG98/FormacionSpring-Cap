8296cd6c8a946d1ce43519fae33dab49e163194b
Alejandro <alejandrogarciav3098@gmail.com>
2023-04-19 14:27:11 +0200

    GetAll Actors - Muestra todos los Actores, faltan operaciones (PUT/Delete)

--- Fundamentos/catalogo_promesas/src/componentes/inicio/Actores/GetAll.js
+++ Fundamentos/catalogo_promesas/src/componentes/inicio/Actores/GetAll.js
@@ -11,18 +11,42 @@ export default class GetAll extends Component {
   componentDidMount() {
     fetch('http://localhost:8001/actores/getAll')
       .then(response => response.json())
-      .then(data => this.setState({ actors: data }));
+      .then(data => {
+        const actors = data.map(actor => {
+          // Separo el nomber completo en: Nombre y Apellido
+          const [firstName, ...lastName] = actor.nombre.toLowerCase().split(' ');
+          // Hago que el primer caracter sea en mayusculas tanto del nomber como del apellido
+          const capitalizedFirstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+          const capitalizedLastName = lastName.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+          // Devolver el nombre completo con el cambio
+          return {
+            ...actor,
+            nombre: capitalizedFirstName + ' ' + capitalizedLastName
+          };
+        });
+        this.setState({ actors });
+      });
   }
 
   render() {
     return (
       <div>
-        <h1>getAll Actors</h1>
-        <ul>
-          {this.state.actors.map(actor => (
-            <li key={actor.id}>{actor.nombre}</li>
-          ))}
-        </ul>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Actores</th>
+              <th scope="col">Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.actors.map(actor => (
+              <tr key={actor.id}>
+                <td>{actor.nombre}</td>
+                <td>---</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
